19341b2642a6dcc0557e16893d08ffe04228da9c
Tom Evans <74tevans@gmail.com>
2024-10-17 11:18:03 +0100

    Added script.js to toggle lightbulbs on/off and update counter. linked script in index.html

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Write your code here
+const lightbulbs = document.querySelectorAll(".row > div");
+const subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+for (let lightbulb of lightbulbs) {
+    lightbulb.addEventListener("click", function(){
+        count++;
+        if (count === 1){
+            subtitle.innerHTML = `You've clicked the lights ${count} time`;
+        }
+        else {
+            subtitle.innerHTML = `You've clicked the lights ${count} times`;
+        }
+        lightbulb.classList.toggle("active");
+    })
+}
